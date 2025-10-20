cd314bfeb0966061eab4566868146155cd9872ae
PostboxRetinal <78338192+PostboxRetinal@users.noreply.github.com>
2025-10-20 00:57:45 +0000

    fix. added missing sql statements for products table

--- init.sql
+++ init.sql
@@ -1,4 +1,3 @@
-
 CREATE DATABASE myflaskapp;
 use myflaskapp;
 
@@ -10,6 +9,17 @@ CREATE TABLE users (
     password varchar(255)
 );
 
+CREATE TABLE product (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name TEXT NOT NULL,
+    description TEXT,
+    price INT NOT NULL
+);
 
 INSERT INTO users VALUES(null, "juan", "juan@gmail.com", "juan", "123"),
     (null, "maria", "maria@gmail.com", "maria", "456");
+
+INSERT INTO product (name, description, price) VALUES
+    ('openSUSE', 'Una distro de Linux muy god', 5000),
+    ('Debian', 'Una distro de Linux muy estable', 3500),
+    ('Manjaro', 'Una distro de Linux muy amigable', 3100);
